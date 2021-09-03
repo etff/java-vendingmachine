58a5ea945e0d4256b64e5438875c9bb1bda2cb1d
etff <wish2rich@gmail.com>
2021-09-03 13:28:01 +0900

    feature: 아이템의 기능을 구현한다

---
+++ src/main/java/vendingmachine/domain/Name.java
@@ -0,0 +1,32 @@
+package vendingmachine.domain;
+
+import java.util.Objects;
+
+public class Name {
+    private static final String NAME_BLANK_NOT_ALLOW = "상품명은 공백이 되어서는 안됩니다.";
+    private String value;
+
+    public Name(String value) {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException(NAME_BLANK_NOT_ALLOW);
+        }
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Name)) return false;
+        Name name = (Name) o;
+        return Objects.equals(getValue(), name.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
+}
---
+++ src/main/java/vendingmachine/domain/Price.java
@@ -0,0 +1,32 @@
+package vendingmachine.domain;
+
+import java.util.Objects;
+
+public class Price {
+    private static final int MINIMUM_PRICE_VALUE = 100;
+    private int value;
+
+    public Price(int value) {
+        if (value < MINIMUM_PRICE_VALUE) {
+            throw new IllegalArgumentException(MINIMUM_PRICE_VALUE + "보다 커야합니다");
+        }
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Price)) return false;
+        Price price = (Price) o;
+        return getValue() == price.getValue();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
+}
