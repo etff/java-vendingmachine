e177e886eb08c74dfebec064b6d3b49508fbca8d
etff <wish2rich@gmail.com>
2021-09-04 11:52:44 +0900

    test: 금액의 테스트를 추가합니다
    - 상품가격 나누기 검증 추가

--- src/main/java/vendingmachine/domain/Price.java
+++ src/main/java/vendingmachine/domain/Price.java
@@ -7,13 +7,22 @@ import java.util.Objects;
  */
 public class Price {
     private static final int MINIMUM_PRICE_VALUE = 100;
+    private static final int MINIMUM_PRICE = 10;
+    private static final String PRICE_MOD_NOT_MATCH = "가격은 10원으로 나누어 떨어져야합니다.";
     private int value;
 
     public Price(int value) {
+        validatePrice(value);
+        this.value = value;
+    }
+
+    private void validatePrice(int value) {
         if (value < MINIMUM_PRICE_VALUE) {
             throw new IllegalArgumentException(MINIMUM_PRICE_VALUE + "보다 커야합니다");
         }
-        this.value = value;
+        if (value % MINIMUM_PRICE != 0) {
+            throw new IllegalArgumentException(PRICE_MOD_NOT_MATCH);
+        }
     }
 
     public Price addPrice(int value) {
